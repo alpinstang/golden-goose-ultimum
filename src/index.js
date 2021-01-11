d16b0157c451fde61213ce6965fe7146c9d568db
John McDonald <jmcdonald1991@gmail.com>
2021-01-10 21:15:39 -0600

    add redux, basic setup, user reducers and actions

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 
 // Strict Mode Documentation: https://reactjs.org/docs/strict-mode.html
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
