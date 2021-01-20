eca7634de0ea85c0af8953de122d4ae278cc3284
John McDonald <jmcdonald1991@gmail.com>
2021-01-20 00:45:59 -0600

    cart in checkout page, total values, checkout item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,12 +5,13 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { withRouter } from 'react-router-dom';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ? (
@@ -21,7 +22,12 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className="empty-message">Your cart is empty.</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push('/checkoout')}>
+    <CustomButton
+      onClick={() => {
+        history.push('/checkoout');
+        dispatch(toggleCartHidden());
+      }}
+    >
       GO TO CHECKOUT
     </CustomButton>
   </div>
