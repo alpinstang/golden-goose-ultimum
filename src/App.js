b91242c9823af8b2139317ee163ea65dd2e4494a
John McDonald <jmcdonald1991@gmail.com>
2021-01-26 19:12:24 -0600

    rename app imports and components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
-import Home from './pages/home/home.component';
-import Shop from './pages/shop/shop.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
+import SignInSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { selectCurrentUser } from './redux/user/user.selector';
+import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
 import CheckoutPage from './pages/checkout/checkout.component';
 
@@ -44,8 +44,8 @@ class App extends React.Component {
       <BrowserRouter>
         <Header />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/shop" component={Shop} />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
           <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
