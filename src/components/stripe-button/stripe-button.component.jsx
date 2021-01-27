1f55466d93f8a345ac32555000b77d6979422318
John McDonald <jmcdonald1991@gmail.com>
2021-01-26 20:26:44 -0600

    alert box sweetalert

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import SweetAlert from 'sweetalert2-react';
 
 const StripeCheckoutButton = ({ price }) => {
+  const [show, setShow] = useState(false);
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey =
+    'pk_test_51ICdr9LN7HUSK1rupJjaRUWHkAcm1ziP0aQV9WYt4Wj2113gLnU2AAqWPHObKeBpdz8cNXhwEs59BsHw01cTpusD007eOhoGu5';
 
-  const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+  const onToken = (token) => {
+    //console.log(token);
+    //alert('Payment Succesful!');
+    setShow(true);
   };
 
   return (
-    <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
-      billingAddress
-      shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
-      description={`Your total is $${price}`}
-      amount={priceForStripe}
-      panelLabel='Pay Now'
-      token={onToken}
-      stripeKey={publishableKey}
-    />
+    <div>
+      <StripeCheckout
+        label="Pay Now"
+        name="GG Clothing Ltd."
+        billingAddress
+        shippingAddress
+        image="https://svgshare.com/i/CUz.svg"
+        description={`Your total is $${price}`}
+        amount={priceForStripe}
+        panelLabel="Pay Now"
+        token={onToken}
+        stripeKey={publishableKey}
+      />
+      <SweetAlert
+        show={show}
+        title="Success!"
+        text="Your payment was successful."
+        onConfirm={() => setShow(false)}
+      />
+    </div>
   );
 };
 
