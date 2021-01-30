c75951c2a7a87a786b30b0dfc48cb68753d1627a
John McDonald <jmcdonald1991@gmail.com>
2021-01-29 23:52:22 -0600

    add some tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,26 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header sign in link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/SIGN IN/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders shop link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/SHOP/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders contact link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/CONTACT/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders directory item', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/HATS/i);
   expect(linkElement).toBeInTheDocument();
 });
