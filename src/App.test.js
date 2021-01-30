6c7fae359317e75647d1ab090e55bb43a513d12f
John McDonald <jmcdonald1991@gmail.com>
2021-01-30 00:00:24 -0600

    app test store wrapper

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 test('renders header sign in link', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
   const linkElement = screen.getByText(/SIGN IN/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders shop link', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
   const linkElement = screen.getByText(/SHOP/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders contact link', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
   const linkElement = screen.getByText(/CONTACT/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders directory item', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
   const linkElement = screen.getByText(/HATS/i);
   expect(linkElement).toBeInTheDocument();
 });
