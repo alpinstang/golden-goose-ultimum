f89f435a9f3fef67146cb5a140109e2978a47a05
John McDonald <jmcdonald1991@gmail.com>
2021-01-31 14:30:48 -0600

    thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
 
